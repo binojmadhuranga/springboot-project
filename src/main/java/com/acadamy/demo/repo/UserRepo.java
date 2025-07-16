343b9468e09fb7c619a2bb484a9ba8b2d3456c7e
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2025-07-16 20:40:42 +0530

    Create a  UserRepo.

---
+++ src/main/java/com/acadamy/demo/repo/UserRepo.java
@@ -0,0 +1,13 @@
+package com.acadamy.demo.repo;
+
+import com.acadamy.demo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Long> {
+
+
+
+
+}

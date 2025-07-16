4dd02f42611692c25e54509ecc48743b3777875f
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2025-07-16 18:00:35 +0530

    first commit

---
+++ src/main/java/com/acadamy/demo/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.acadamy.demo.controller;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/v1/user")
+@CrossOrigin
+@RestController
+
+public class UserController {
+
+    @GetMapping("get")
+    public  String test() {
+        return "Hello from UserController";
+    }
+
+
+
+}

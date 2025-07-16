ff056a962842319792a66564c8ed93e306dc7173
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2025-07-16 22:15:27 +0530

    Develop Save method.

--- src/main/java/com/acadamy/demo/controller/UserController.java
+++ src/main/java/com/acadamy/demo/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.acadamy.demo.controller;
 
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.acadamy.demo.dto.ResponseDto;
+import com.acadamy.demo.dto.UserDto;
+import com.acadamy.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api/v1/user")
 @CrossOrigin
@@ -12,11 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class UserController {
 
-    @GetMapping("get")
-    public  String test() {
-        return "Hello from UserController";
-    }
+//    @GetMapping("get")
+//    public  String test() {
+//        return "Hello from UserController";
+//    }
 
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/save")
+    public ResponseEntity<ResponseDto> saveUser(@RequestBody UserDto userDto) {
+        return userService.saveUser(userDto);
+    }
 
 
 }
--- src/main/java/com/acadamy/demo/service/impl/UserServiceIMPL.java
+++ src/main/java/com/acadamy/demo/service/impl/UserServiceIMPL.java
@@ -2,15 +2,30 @@ package com.acadamy.demo.service.impl;
 
 import com.acadamy.demo.dto.ResponseDto;
 import com.acadamy.demo.dto.UserDto;
+import com.acadamy.demo.entity.User;
+import com.acadamy.demo.repo.UserRepo;
 import com.acadamy.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceIMPL implements UserService {
 
+    @Autowired
+    private UserRepo userRepo;
+
     @Override
     public ResponseEntity<ResponseDto> saveUser(UserDto userDto) {
-        return null;
+        User user = new User();
+        user.setName(userDto.getName());
+        user.setAge(userDto.getAge());
+        userRepo.save(user);
+
+        return new ResponseEntity<>(ResponseDto.builder().
+                message("User saved successfully").
+                data(userDto).
+                build(), HttpStatus.CREATED);
     }
 }

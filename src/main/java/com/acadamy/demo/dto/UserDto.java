92ceaf701ad5797884d499ab25ef632233afec88
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2025-07-16 19:26:33 +0530

    Create a DTO.

---
+++ src/main/java/com/acadamy/demo/dto/UserDto.java
@@ -0,0 +1,22 @@
+package com.acadamy.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+
+    private String name;
+
+    private int age;
+
+
+
+
+
+
+
+}
--- src/main/java/com/acadamy/demo/entity/User.java
+++ src/main/java/com/acadamy/demo/entity/User.java
@@ -1,10 +1,14 @@
 package com.acadamy.demo.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     @Id
